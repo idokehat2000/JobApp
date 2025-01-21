177b9b5cc7c1df3bdd348796943b75a99885c485
idokehat <idokehat@gmail.com>
2025-01-21 10:31:29 +0200

    Works!

--- src/main/java/com/example/JobApp/Model/JobPost.java
+++ src/main/java/com/example/JobApp/Model/JobPost.java
@@ -27,4 +27,40 @@ public class JobPost {
         this.reqExperience = reqExperience;
         this.postTechStack = postTechStack;
     }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public String getPostProfile() {
+        return postProfile;
+    }
+
+    public void setPostProfile(String postProfile) {
+        this.postProfile = postProfile;
+    }
+
+    public String getPostDesc() {
+        return postDesc;
+    }
+
+    public void setPostDesc(String postDesc) {
+        this.postDesc = postDesc;
+    }
+
+    public Integer getReqExperience() {
+        return reqExperience;
+    }
+
+    public void setReqExperience(Integer reqExperience) {
+        this.reqExperience = reqExperience;
+    }
+
+    public List<String> getPostTechStack() {
+        return postTechStack;
+    }
+
+    public void setPostTechStack(List<String> postTechStack) {
+        this.postTechStack = postTechStack;
+    }
 }
